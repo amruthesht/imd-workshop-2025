6a038201fee9a4b41f15dd7236debf89b4592d34
Matthias Heyden <95201808+HeydenLabASU@users.noreply.github.com>
2025-11-09 08:12:26 -0800

    Add files via upload

--- demos/GROMACS/MSD/md/run.sh
+++ demos/GROMACS/MSD/md/run.sh
@@ -6,10 +6,10 @@ source /usr/local/gromacs-imd-v3/bin/GMXRC
 mkdir -p imd
 cd imd
 echo "running grompp"
-gmx grompp -f ../imd.mdp -c ../start.gro -p ../topol.top -o mda.tpr >& grompp.out
+gmx grompp -f ../imd.mdp -c ../start.gro -p ../topol.top -o imd.tpr >& grompp.out
 echo "starting mdrun"
 if [ ! -z $1 ]; then port=$1
 else port=8888; fi
-gmx mdrun -s mda.tpr -v -nt 1 -pin on -imdwait -imdport ${port}
+gmx mdrun -s imd.tpr -v -nt 1 -pin on -imdwait -imdport ${port}
 cd ..
 rm -r imd
